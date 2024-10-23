f2af48bb305b5e663f9f61a1ac01bd6927441e40
Thian <thian.gg23@gmail.com>
2024-10-23 09:59:47 -0500

    Update Inicio.vue

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -15,23 +15,42 @@
         El JW Marriott es un símbolo de lujo y confort en la hospitalidad moderna. Desde su inauguración, ha ofrecido un servicio excepcional y un ambiente elegante que atrae a viajeros de todo el mundo. Cada hotel JW Marriott es único, reflejando la cultura local y proporcionando experiencias inolvidables.
       </p>
     </div>
-    <div class="horizontal-card">
-      <div class="image-section">
-        <img
-          :src="horizontalImages[currentHorizontalImage]"
-          alt="Horizontal Image"
-          class="horizontal-image"
-        />
+
+    <div class="cards-container">
+      <div class="horizontal-card">
+        <div class="image-section">
+          <img
+            :src="horizontalImages[currentHorizontalImage]"
+            alt="Horizontal Image"
+            class="horizontal-image"
+          />
+        </div>
+        <div class="description-section">
+          <h2>Servicios</h2>
+          <p>
+            El JW Marriott ofrece una experiencia de lujo con una amplia variedad de servicios diseñados para tu comodidad y bienestar. Relájate y disfruta de nuestra piscina de última generación, sumérgete en la tranquilidad de nuestro sauna, o date un merecido descanso en el jacuzzi. Además, contamos con un spa exclusivo que ofrece tratamientos rejuvenecedores y una atención personalizada para revitalizar cuerpo y mente.
+            Entre otros servicios, el JW Marriott se destaca por su restaurante gourmet, gimnasio de alta gama y actividades recreativas pensadas para brindarte una estancia inolvidable.
+            Para más información, haz clic <router-link to="/servicios">aquí</router-link>.
+          </p>
+        </div>
       </div>
-      <div class="description-section">
-        <h2>Sipnosis</h2>
-        <p>
-          El JW Marriott ofrece una experiencia de lujo con una amplia variedad de servicios diseñados para tu comodidad y bienestar. Relájate y disfruta de nuestra piscina de última generación, sumérgete en la tranquilidad de nuestro sauna, o date un merecido descanso en el jacuzzi. Además, contamos con un spa exclusivo que ofrece tratamientos rejuvenecedores y una atención personalizada para revitalizar cuerpo y mente.
 
-          Entre otros servicios, el JW Marriott se destaca por su restaurante gourmet, gimnasio de alta gama y actividades recreativas pensadas para brindarte una estancia inolvidable.
-
-          Para más información, haz clic <router-link to="/servicios">aquí</router-link>.
-        </p>
+      <div class="horizontal-card fixed-card">
+        <div class="image-section">
+          <img
+            :src="activityImages[currentActivityImage]"
+            alt="Activity Image"
+            class="horizontal-image"
+          />
+        </div>
+        <div class="description-section">
+          <h2>Actividades</h2>
+          <p>
+            El JW Marriott te invita a vivir una aventura sin igual con una amplia variedad de actividades de deportes extremos que desafiarán tus límites. Atrévete a experimentar el adrenalínico canyoning en ríos cristalinos, descubre la belleza oculta del espeleísmo en impresionantes cuevas, o siente la emoción del bungee jump desde alturas vertiginosas. También puedes disfrutar de la emoción del rafting en rápidos desafiantes que pondrán a prueba tu valentía.
+            Entre estas emocionantes experiencias, el JW Marriott se enorgullece de ofrecerte la oportunidad de conectar con la naturaleza mientras disfrutas de un ambiente seguro y confortable.
+            Para más información, haz clic <router-link to="/Actividades">aquí</router-link>.
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -49,15 +68,24 @@ const images = [
 ];
 
 const horizontalImages = [
-  'https://img.freepik.com/fotos-premium/modelo-fitness-joven-hermosa-chica-morena-traje-bano-negro-posa-piscina-agua_115086-1386.jpg', // piscina
-  'https://www.fluidra.com/projects//web/app/uploads/2021/08/iStock-827324092.jpg', // sauna
-  'https://www.firplak.com/wp-content/uploads/2024/05/Hidromasaje-Santorini-Empotrar-Blanco-Iso-WEB.webp', // jacuzzi
-  'https://www.bellatriz.com/wp-content/uploads/2022/04/woman-relaxing-in-the-spa-scaled.jpg', // spa
-  'https://www.collinsdictionary.com/images/full/restaurant_135621509.jpg', // restaurante
+  'https://img.freepik.com/fotos-premium/modelo-fitness-joven-hermosa-chica-morena-traje-bano-negro-posa-piscina-agua_115086-1386.jpg',
+  'https://www.fluidra.com/projects//web/app/uploads/2021/08/iStock-827324092.jpg',
+  'https://www.firplak.com/wp-content/uploads/2024/05/Hidromasaje-Santorini-Empotrar-Blanco-Iso-WEB.webp',
+  'https://www.bellatriz.com/wp-content/uploads/2022/04/woman-relaxing-in-the-spa-scaled.jpg',
+  'https://www.collinsdictionary.com/images/full/restaurant_135621509.jpg',
+];
+
+const activityImages = [
+  'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/07/b1/bb/61.jpg',
+  'https://images.mnstatic.com/b6/d6/b6d67b7e402c48a351e30ca76f02e9ac.jpg',
+  'https://www.boyacaradio.com/imagenes/fotos_noticias/zero-gravity-parapente-algodonales.jpg',
+  'https://paravolarcolombia.com/wp-content/uploads/2022/09/Bungee-140-mts-Ecolombia-Experience-6.jpg',
+  'https://paravolarcolombia.com/wp-content/uploads/2022/09/espeliologia-en-santander-1024x683.jpg',
 ];
 
 const currentImage = ref(0);
 const currentHorizontalImage = ref(0);
+const currentActivityImage = ref(0);
 const showCard = ref(false);
 
 const changeImage = () => {
@@ -68,9 +96,14 @@ const changeHorizontalImage = () => {
   currentHorizontalImage.value = (currentHorizontalImage.value + 1) % horizontalImages.length;
 };
 
+const changeActivityImage = () => {
+  currentActivityImage.value = (currentActivityImage.value + 1) % activityImages.length;
+};
+
 onMounted(() => {
   setInterval(changeImage, 3000);
   setInterval(changeHorizontalImage, 4000);
+  setInterval(changeActivityImage, 5000);
   setTimeout(() => {
     showCard.value = true;
   }, 2000);
@@ -121,7 +154,6 @@ const closeCard = () => {
   left: 15%; 
   transform: translate(-50%, -75%);
   width: 350px;
-  height: auto;
   max-height: 500px;
   padding: 15px;
   background-color: rgba(52, 58, 64, 0.8);
@@ -129,14 +161,15 @@ const closeCard = () => {
   border-radius: 10px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
   opacity: 0;
-  transform: translate(-50%, -50%) translateY(20px);
   transition: opacity 0.5s ease-in, transform 0.5s cubic-bezier(0.25, 1.5, 0.5, 1);
   z-index: 1000;
 }
 
 .floating-card.show {
   opacity: 1;
+  background-color: rgba(52, 58, 64, 0.8);
   transform: translate(-50%, -50%) translateY(0);
+  font-size: 1.3em;
 }
 
 .floating-card h2,
@@ -146,11 +179,16 @@ const closeCard = () => {
   font-family: 'Georgia', serif;
 }
 
+.cards-container {
+  position: absolute; 
+  top: 2%; 
+  right: 2%; 
+  display: flex;
+  flex-direction: column; 
+  gap: 20px; 
+}
+
 .horizontal-card {
-  position: absolute;
-  bottom: 20px;
-  right: 30%;
-  transform: translateX(50%);
   display: flex;
   background-color: rgba(52, 58, 64, 0.8);
   border: 2px solid #6c757d;
@@ -158,7 +196,6 @@ const closeCard = () => {
   padding: 15px;
   max-width: 800px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
-  z-index: 999;
 }
 
 .image-section {
